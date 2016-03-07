57fcdf12b29a90543a50a74d5c8072806a6e15a1
Olajide Obasan <jideobs@gmail.com>
2016-03-07 03:02:43 +0100

    UsersResources method handlers implementation.

--- server/resources/users.py
+++ server/resources/users.py
@@ -1,12 +1,28 @@
 from flask_restful import Resource
+from flask_restful import abort
+from server.models.users import Users
 
 
 class UsersResource(Resource):
-    def get(self):
-        pass
+	@Users.method()
+	def post(self, user):
+		user.put()
+		return user
 
-    def put(self):
-        pass
+	@Users.method()
+	def put(self, user):
+		if not user.from_datastore:
+			abort(400, message='User does not exist')
+		user.put()
+		return user
 
-    def delete(self):
-        pass
+	@Users.method()
+	def delete(self, user):
+		if not user.from_datastore:
+			abort(400, message='User does not exist')
+		user.key.delete()
+		return user
+
+	@Users.query_method()
+	def get(self, user):
+		return user
