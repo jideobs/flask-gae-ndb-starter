705bff067a5174b688d85f6edf76377e1964b120
Olajide Obasan <jideobs@gmail.com>
2016-03-07 01:56:48 +0100

    resource & routes definition for TasksResource.

--- server/main.py
+++ server/main.py
@@ -6,9 +6,11 @@ from flask_login import LoginManager, login_user, login_required, current_user, 
 from models.users import Users
 from config import APP_CONFIG
 from webapp2_extras import security
-from resources.users import UsersResource
 from forms import LoginForm
 from forms import RegisterFormExt
+
+from resources.users import UsersResource
+from resources.tasks import TasksResource
 
 app = Flask(__name__)
 app.config.update(APP_CONFIG)
@@ -21,58 +23,59 @@ login_manager.login_message = 'Please login to access this page.'
 
 @login_manager.user_loader
 def user_loader(username):
-    return Users.get_by_username(username)
+	return Users.get_by_username(username)
 
 
 @app.route('/', methods=['GET'])
 def default():
-    return render_template('index.html')
+	return render_template('index.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated:
-        return redirect('dashboard')
+	if current_user.is_authenticated:
+		return redirect('dashboard')
 
-    form = LoginForm(csrf_enabled=False)
-    if request.method == 'POST' and form.validate_on_submit():
-        form.user.is_authenticated = True
-        form.user.put()
-        login_user(form.user, remember=form.remember_me.data)
-        sleep(1)
-        return redirect(url_for('dashboard'))
-    else:
-        return render_template('login.html', form=form)
+	form = LoginForm(csrf_enabled=False)
+	if request.method == 'POST' and form.validate_on_submit():
+		form.user.is_authenticated = True
+		form.user.put()
+		login_user(form.user, remember=form.remember_me.data)
+		sleep(1)
+		return redirect(url_for('dashboard'))
+	else:
+		return render_template('login.html', form=form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    register_error = None
-    form = RegisterFormExt(csrf_enabled=False)
-    if request.method == 'POST' and form.validate():
-        password = security.generate_password_hash(form.password.data, length=32)
-        user = Users(username=form.username.data, password=password)
-        user.put()
-        return redirect(url_for('login'), code=302)
-    return render_template('register.html', form=form, register_error=register_error)
+	register_error = None
+	form = RegisterFormExt(csrf_enabled=False)
+	if request.method == 'POST' and form.validate():
+		password = security.generate_password_hash(form.password.data, length=32)
+		user = Users(username=form.username.data, password=password)
+		user.put()
+		return redirect(url_for('login'), code=302)
+	return render_template('register.html', form=form, register_error=register_error)
 
 
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
-    return render_template('dashboard.html')
+	return render_template('dashboard.html')
 
 
 @app.route('/logout', methods=['GET'])
 @login_required
 def logout():
-    current_user.is_authenticated = False
-    current_user.put()
-    logout_user()
-    return redirect(url_for('login'))
+	current_user.is_authenticated = False
+	current_user.put()
+	logout_user()
+	return redirect(url_for('login'))
 
 
 api.add_resource(UsersResource, '/users', '/users/<string:id>')
+api.add_resource(TasksResource, '/tasks', '/tasks/<string:id>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
