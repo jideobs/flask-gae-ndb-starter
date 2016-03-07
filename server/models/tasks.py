c63f6cf4c8fe358efb8acf2d56539ffa2df95a7a
Olajide Obasan <jideobs@gmail.com>
2016-03-07 02:59:02 +0100

    Tasks properties definition.

--- server/models/tasks.py
+++ server/models/tasks.py
@@ -1,10 +1,9 @@
 from google.appengine.ext import ndb
-
 from model_base import ModelBase
 from users import Users
 
 
 class Tasks(ModelBase):
-    owner = ndb.KeyProperty(kind=Users, required=True)
-    title = ndb.StringProperty(required=True)
-    date_completed = ndb.DateTimeProperty(auto_now_add=True)
+	owner = ndb.KeyProperty(kind=Users, required=True)
+	title = ndb.StringProperty(required=True)
+	date_completed = ndb.DateTimeProperty(auto_now_add=True)
